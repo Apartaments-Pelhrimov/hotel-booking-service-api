df656796e9e2381a0834ccfc2c641d8ee6bcd611
mibal_ua <9mohapx9@gmail.com>
2023-11-06 13:35:20 +0100

    Implement `RegistrationDto` with validations

---
+++ src/main/java/ua/mibal/booking/model/dto/RegistrationDto.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Value;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@Value
+public class RegistrationDto {
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    String name;
+
+    @NotNull
+    @Pattern(regexp = "\\+([0-9]){10,12}$")
+    String phone;
+
+    @NotNull
+    @Email
+    String email;
+
+    @NotBlank
+    @Size(min = 8, max = 100)
+    String password;
+}
