c328fda28736bef05ac4354798cbcc3c6fc4856d
mibal_ua <9mohapx9@gmail.com>
2023-11-06 13:46:22 +0100

    Implement `RegistrationController`

---
+++ src/main/java/ua/mibal/booking/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ua.mibal.booking.model.dto.RegistrationDto;
+import ua.mibal.booking.service.RegistrationService;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@RequiredArgsConstructor
+@RestController
+public class RegistrationController {
+    private final RegistrationService registrationService;
+
+    @PostMapping("/registration")
+    public void register(@Valid @RequestBody RegistrationDto registrationDto) {
+        registrationService.register(registrationDto);
+    }
+}
