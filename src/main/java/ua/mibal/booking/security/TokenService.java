7b0118afd1999fd27474bf918b850f6717f09b68
mibal_ua <9mohapx9@gmail.com>
2023-11-07 10:47:34 +0100

    Implement `TokenService`

---
+++ src/main/java/ua/mibal/booking/security/TokenService.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+import ua.mibal.booking.model.entity.User;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final JwtEncoder jwtEncoder;
+
+    @Value(value = "${jwt-token.expiring-days}")
+    private Integer tokenExpiresInDays = 10;
+
+    public String generateToken(Authentication authentication) {
+        return generateToken(authentication.getName(), authentication.getAuthorities());
+    }
+
+    public String generateToken(User user) {
+        return generateToken(user.getName(), user.getAuthorities());
+    }
+
+    private String generateToken(String name, Collection<? extends GrantedAuthority> grantedAuthorities) {
+        Instant now = Instant.now();
+        String scope = grantedAuthorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(tokenExpiresInDays, ChronoUnit.DAYS))
+                .subject(name)
+                .claim("scope", scope)
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims))
+                .getTokenValue();
+    }
+}
