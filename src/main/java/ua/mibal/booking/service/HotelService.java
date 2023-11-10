5bf9d0ce2441ab260736f256ea763a5fceebb433
mibal_ua <9mohapx9@gmail.com>
2023-11-10 10:29:07 +0100

    Fix codestyle

--- src/main/java/ua/mibal/booking/service/HotelService.java
+++ src/main/java/ua/mibal/booking/service/HotelService.java
@@ -37,8 +37,7 @@ public class HotelService {
     private final HotelRepository hotelRepository;
     private final HotelMapper hotelMapper;
 
-    public Page<HotelSearchDto> findAll(Request request,
-                                        Pageable pageable) {
+    public Page<HotelSearchDto> findAll(Request request, Pageable pageable) {
         return hotelRepository.findAllByQuery(request, pageable)
                 .map(hotel -> hotelMapper
                         .toDto(hotel, null, null));
