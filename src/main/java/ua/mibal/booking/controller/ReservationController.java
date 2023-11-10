6a81ef423e2f8863fb1af91f3c07824ff4026ce1
mibal_ua <9mohapx9@gmail.com>
2023-11-10 17:16:49 +0100

    Implement `ReservationController` `getMyReservations`

---
+++ src/main/java/ua/mibal/booking/controller/ReservationController.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.mibal.booking.model.dto.ReservationDto;
+import ua.mibal.booking.service.ReservationService;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @RolesAllowed("USER")
+    @GetMapping("/me/reservations")
+    public ReservationDto getMyReservations(Authentication authentication) {
+        return reservationService.getMyReservations(authentication);
+    }
+}
