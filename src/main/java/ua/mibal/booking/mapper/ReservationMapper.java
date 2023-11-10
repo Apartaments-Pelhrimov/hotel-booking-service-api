da2163db3a8ad6b556087184dabe3f10da119fa5
mibal_ua <9mohapx9@gmail.com>
2023-11-10 17:37:41 +0100

    Implement `ReservationMapper`

---
+++ src/main/java/ua/mibal/booking/mapper/ReservationMapper.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import ua.mibal.booking.model.dto.ReservationDto;
+import ua.mibal.booking.model.entity.Reservation;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ReservationMapper {
+
+    @Mapping(target = "date", source = "dateTime")
+    @Mapping(target = "apartment.hotelName", source = "apartment.hotel.name")
+    @Mapping(target = "apartment.location", source = "apartment.hotel.location")
+    ReservationDto toDto(Reservation reservation);
+}
