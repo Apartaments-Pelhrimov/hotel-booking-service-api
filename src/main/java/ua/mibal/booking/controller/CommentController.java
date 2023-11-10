47ca3198b1bee33c2b2fe056227ce0193463ba7b
mibal_ua <9mohapx9@gmail.com>
2023-11-10 10:13:03 +0100

    Implement `CommentController.getCommentsForApartment` and `.getCommentsForHotel`

---
+++ src/main/java/ua/mibal/booking/controller/CommentController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.mibal.booking.model.dto.CommentDto;
+import ua.mibal.booking.service.CommentService;
+
+import java.util.List;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/hotels")
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/{hotelId}/comments")
+    public List<CommentDto> getCommentsForHotel(@PathVariable Long hotelId, Pageable pageable) {
+        return commentService.getCommentsByHotelId(hotelId, pageable);
+    }
+
+    @GetMapping("apartments/{apartmentId}/comments")
+    public List<CommentDto> getCommentsForApartment(@PathVariable Long apartmentId, Pageable pageable) {
+        return commentService.getCommentsByApartmentId(apartmentId, pageable);
+    }
+}
---
+++ src/main/java/ua/mibal/booking/service/CommentService.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import ua.mibal.booking.model.dto.CommentDto;
+
+import java.util.List;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    public List<CommentDto> getCommentsByHotelId(Long hotelId, Pageable pageable) {
+        return null;
+    }
+
+    public List<CommentDto> getCommentsByApartmentId(Long apartmentId, Pageable pageable) {
+        return null;
+    }
+}
