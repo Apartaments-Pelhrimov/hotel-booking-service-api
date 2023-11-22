65d320763ed1a71c99f3b798dcca8478a5274872
mibal_ua <9mohapx9@gmail.com>
2023-11-22 12:11:01 +0100

    Add `S3Client` bean
    and add AWS s3 sdk dependency

---
+++ src/main/java/ua/mibal/booking/config/properties/AwsProps.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023. Mykhailo Balakhon, mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import software.amazon.awssdk.regions.Region;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">email</a>
+ */
+@ConfigurationProperties(prefix = "aws")
+public record AwsProps(
+        String accessKeyId,
+        String secretAccessKey,
+        Region region,
+        String bucketName
+) {
+}
