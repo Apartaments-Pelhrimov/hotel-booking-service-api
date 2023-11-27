f2968edd175470f011df59c6c6072265f339ad87
mibal_ua <9mohapx9@gmail.com>
2023-11-27 11:12:08 +0100

    Add `ApartmentClass` field

--- src/main/java/ua/mibal/booking/model/entity/ApartmentType.java
+++ src/main/java/ua/mibal/booking/model/entity/ApartmentType.java
@@ -22,6 +22,8 @@ import jakarta.persistence.Convert;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -96,6 +98,10 @@ public class ApartmentType {
     @Column
     private Double rating;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ApartmentClass apartmentClass;
+
     @ElementCollection
     @BatchSize(size = 100)
     @CollectionTable(
@@ -166,5 +172,9 @@ public class ApartmentType {
 
     public boolean deletePhoto(Photo photo) {
         return this.photos.remove(photo);
+    }
+
+    public enum ApartmentClass {
+        COMFORT, STANDARD
     }
 }
