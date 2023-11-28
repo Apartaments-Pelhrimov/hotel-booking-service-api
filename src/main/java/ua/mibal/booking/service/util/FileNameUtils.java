cae890ffdbc222cc0bc7240300e2021ec49f93f2
mibal_ua <9mohapx9@gmail.com>
2023-11-28 10:18:32 +0100

    Add `null` case

--- src/main/java/ua/mibal/booking/service/util/FileNameUtils.java
+++ src/main/java/ua/mibal/booking/service/util/FileNameUtils.java
@@ -4,6 +4,8 @@ import org.springframework.util.StringUtils;
 import ua.mibal.booking.model.exception.IllegalPhotoFormatException;
 import ua.mibal.booking.service.photo.PhotoExtension;
 
+import java.util.Objects;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">email</a>
@@ -14,8 +16,8 @@ public class FileNameUtils {
         String extension = null;
         try {
             extension = StringUtils.getFilenameExtension(fileName);
-            return PhotoExtension.valueOf(extension.toUpperCase());
-        } catch (IllegalArgumentException e) {
+            return PhotoExtension.valueOf(Objects.requireNonNull(extension).toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
             throw new IllegalPhotoFormatException(extension);
         }
     }
