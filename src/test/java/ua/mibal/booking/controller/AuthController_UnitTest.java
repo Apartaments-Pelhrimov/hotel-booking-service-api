41dceb952218f7658ac4143bd2a9241381877847
mibal_ua <9mohapx9@gmail.com>
2023-11-30 20:04:03 +0100

    Implement absolutely `AuthController` tests

--- src/test/java/ua/mibal/booking/controller/AuthController_UnitTest.java
+++ src/test/java/ua/mibal/booking/controller/AuthController_UnitTest.java
@@ -18,15 +18,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import ua.mibal.booking.model.dto.auth.AuthResponseDto;
+import ua.mibal.booking.model.dto.auth.ForgetPasswordDto;
 import ua.mibal.booking.model.dto.auth.RegistrationDto;
 import ua.mibal.booking.service.AuthService;
 import ua.mibal.booking.testUtils.RegistrationDtoArgumentConverter;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -112,5 +115,91 @@ class AuthController_UnitTest {
                 .andExpect(status().isOk());
         verify(authService, times(1))
                 .register(registrationDto);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"code", "27kRW,2FUvh$4zghVEF8GO0uJ=~e=A"})
+    void activate_should_delegate_activation_code_to_AuthService(String code) throws Exception {
+        mvc.perform(post("/api/auth/activate")
+                        .param("code", code))
+                .andExpect(status().isOk());
+        verify(authService, times(1))
+                .activate(code);
+    }
+
+    @Test
+    void activate_should_throw_exception_if_code_was_not_passed() throws Exception {
+        mvc.perform(post("/api/auth/activate"))
+                .andExpect(status().isBadRequest());
+        verify(authService, never())
+                .activate(any());
+    }
+
+    @Test
+    void resetPassword_should_throw_exception_if_code_was_not_passed() throws Exception {
+        mvc.perform(get("/api/auth/forget"))
+                .andExpect(status().isBadRequest());
+        verify(authService, never())
+                .restore(any());
+    }
+
+    @Test
+    void resetPassword_should_delegate_email_to_AuthService() throws Exception {
+        mvc.perform(get("/api/auth/forget")
+                        .param("email", "email"))
+                .andExpect(status().isOk());
+        verify(authService, times(1))
+                .restore("email");
+    }
+
+    @Test
+    void newPassword_should_throw_exception_if_code_was_not_passed() throws Exception {
+        mvc.perform(post("/api/auth/forget/new"))
+                .andExpect(status().isBadRequest());
+        verify(authService, never())
+                .newPassword(any(), any());
+    }
+
+    @Test
+    void newPassword_should_throw_exception_if_request_body_is_empty() throws Exception {
+        mvc.perform(post("/api/auth/forget/new")
+                        .param("code", "code"))
+                .andExpect(status().isBadRequest());
+        verify(authService, never())
+                .newPassword(any(), any());
+    }
+
+    @Test
+    void newPassword_should_delegate_params_to_AuthService() throws Exception {
+        ForgetPasswordDto forgetPasswordDto = new ForgetPasswordDto("password1");
+
+        mvc.perform(post("/api/auth/forget/new")
+                        .param("code", "code")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(forgetPasswordDto)))
+                .andExpect(status().isOk());
+        verify(authService, times(1))
+                .newPassword("code", forgetPasswordDto);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "null, null",
+            "code, null",
+            "null, aaaa1234",
+            "code, aaaaaaa",
+            "27kRW2FUvh$4zghVEF8GO0uJ=~e=A, 1234567",
+            "code, aaa123"
+    }, nullValues = "null")
+    void newPassword_should_throw_ValidationException_while_pass_incorrect_ForgetPasswordDto(String code, String password) throws Exception {
+        ForgetPasswordDto forgetPasswordDto = new ForgetPasswordDto(password);
+
+        mvc.perform(post("/api/auth/forget/new")
+                        .param("code", code)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(forgetPasswordDto)))
+                .andExpect(status().isBadRequest());
+        verify(authService, never())
+                .newPassword(code, forgetPasswordDto);
     }
 }
