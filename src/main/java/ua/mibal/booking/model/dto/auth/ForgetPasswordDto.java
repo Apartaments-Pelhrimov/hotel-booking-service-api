00d145a57a1c7aa3a1d0f2aa04235f856a891f0f
mibal_ua <9mohapx9@gmail.com>
2023-11-30 20:04:37 +0100

    Fix `ForgetPasswordDto` validation annotations

--- src/main/java/ua/mibal/booking/model/dto/auth/ForgetPasswordDto.java
+++ src/main/java/ua/mibal/booking/model/dto/auth/ForgetPasswordDto.java
@@ -1,10 +1,11 @@
 package ua.mibal.booking.model.dto.auth;
 
+import jakarta.validation.constraints.NotNull;
 import ua.mibal.booking.model.validation.constraints.Password;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">email</a>
  */
-public record ForgetPasswordDto(@Password String password) {
+public record ForgetPasswordDto(@NotNull @Password String password) {
 }
