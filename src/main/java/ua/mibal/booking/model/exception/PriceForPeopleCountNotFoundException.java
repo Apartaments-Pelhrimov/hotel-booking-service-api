a1d6ffc1f827e2982169100f7e48af927585fcba
mibal_ua <9mohapx9@gmail.com>
2023-12-04 14:57:12 +0100

    Implement own `NotFoundException`

--- src/main/java/ua/mibal/booking/model/exception/PriceForPeopleCountNotFoundException.java
+++ src/main/java/ua/mibal/booking/model/exception/PriceForPeopleCountNotFoundException.java
@@ -1,10 +1,10 @@
-package ua.mibal.booking.model.exception.entity;
+package ua.mibal.booking.model.exception;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
-public class PriceForPeopleCountNotFoundException extends EntityNotFoundException {
+public class PriceForPeopleCountNotFoundException extends NotFoundException {
 
     public PriceForPeopleCountNotFoundException(Long apartmentId, Integer people) {
         super("Apartment with id=" + apartmentId + " does not have options for " + people + " people.");
