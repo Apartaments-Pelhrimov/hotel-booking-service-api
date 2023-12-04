c9e9eadf7afd87bd0cfacf770bc9c5a2e98b8395
mibal_ua <9mohapx9@gmail.com>
2023-12-04 12:16:08 +0100

    Implement `HotelTurningOffTime` entity

---
+++ src/main/java/ua/mibal/booking/model/entity/HotelTurningOffTime.java
@@ -0,0 +1,36 @@
+package ua.mibal.booking.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "hotel_turning_off_times")
+public class HotelTurningOffTime {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime from;
+
+    @Column(nullable = false)
+    private LocalDateTime to;
+}
