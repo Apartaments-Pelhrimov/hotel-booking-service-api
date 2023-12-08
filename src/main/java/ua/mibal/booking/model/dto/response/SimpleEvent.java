16e0ba95e9ba989f591a313a594e4ed14af2982e
mibal_ua <9mohapx9@gmail.com>
2023-12-08 18:00:50 +0100

    Implement `equals` and `hashCode` for `SimpleEvent`

--- src/main/java/ua/mibal/booking/model/dto/response/SimpleEvent.java
+++ src/main/java/ua/mibal/booking/model/dto/response/SimpleEvent.java
@@ -20,5 +20,25 @@ public class SimpleEvent implements Event {
     private LocalDateTime start;
     private LocalDateTime end;
     private String eventName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SimpleEvent that = (SimpleEvent) o;
+
+        if (!start.equals(that.start)) return false;
+        if (!end.equals(that.end)) return false;
+        return eventName.equals(that.eventName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start.hashCode();
+        result = 31 * result + end.hashCode();
+        result = 31 * result + eventName.hashCode();
+        return result;
+    }
 }
 
