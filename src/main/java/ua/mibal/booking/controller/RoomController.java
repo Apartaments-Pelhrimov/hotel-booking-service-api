e32dfce0340e85fa9d537df8b36d557284ace047
mibal_ua <9mohapx9@gmail.com>
2024-01-12 13:31:39 +0100

    Split `ApartmentController` endpoints into other controllers
    Into: `ApartmentInstanceController`, `PriceController`, `RoomController`

--- src/main/java/ua/mibal/booking/controller/RoomController.java
+++ src/main/java/ua/mibal/booking/controller/RoomController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Mykhailo Balakhon mailto:9mohapx9@gmail.com
+ * Copyright (c) 2024. Mykhailo Balakhon mailto:9mohapx9@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,21 +21,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ua.mibal.booking.model.dto.request.ChangeApartmentDto;
-import ua.mibal.booking.model.dto.request.CreateApartmentDto;
-import ua.mibal.booking.model.dto.response.ApartmentCardDto;
-import ua.mibal.booking.model.dto.response.ApartmentDto;
+import ua.mibal.booking.model.dto.request.RoomDto;
 import ua.mibal.booking.service.ApartmentService;
-
-import java.util.List;
 
 /**
  * @author Mykhailo Balakhon
@@ -44,38 +37,22 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/apartments")
-public class ApartmentController {
+public class RoomController {
+    // TODO create RoomService
     private final ApartmentService apartmentService;
 
-    @GetMapping("/{id}")
-    public ApartmentDto getOne(@PathVariable Long id) {
-        return apartmentService.getOneDto(id);
-    }
-
-    @GetMapping
-    public List<ApartmentCardDto> getAll() {
-        return apartmentService.getAll();
-    }
-
     @RolesAllowed("MANAGER")
-    @PostMapping
+    @PostMapping("/{apartmentId}/rooms")
     @ResponseStatus(HttpStatus.CREATED)
-    public void create(@RequestBody @Valid CreateApartmentDto createApartmentDto) {
-        apartmentService.createApartment(createApartmentDto);
+    public void addRoom(@PathVariable Long apartmentId,
+                        @RequestBody @Valid RoomDto roomDto) {
+        apartmentService.addRoom(apartmentId, roomDto);
     }
 
     @RolesAllowed("MANAGER")
-    @PatchMapping("/{id}")
+    @DeleteMapping("/rooms/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void change(@PathVariable Long id,
-                       @RequestBody @Valid ChangeApartmentDto changeApartmentDto) {
-        apartmentService.update(changeApartmentDto, id);
-    }
-
-    @RolesAllowed("MANAGER")
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        apartmentService.delete(id);
+    public void deleteRoom(@PathVariable Long id) {
+        apartmentService.deleteRoom(id);
     }
 }
