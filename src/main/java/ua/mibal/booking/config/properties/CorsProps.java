021d06f32ba24b4a92e680e8100418d514be9454
mibal_ua <9mohapx9@gmail.com>
2024-02-02 15:11:35 +0100

    Fix `CorsProps` validation

--- src/main/java/ua/mibal/booking/config/properties/CorsProps.java
+++ src/main/java/ua/mibal/booking/config/properties/CorsProps.java
@@ -18,11 +18,13 @@ package ua.mibal.booking.config.properties;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
+@Validated
 @ConfigurationProperties("cors")
 public record CorsProps(
         @NotNull
