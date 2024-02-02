1a119c509a4935e58e7956d06d5663917c67ff69
mibal_ua <9mohapx9@gmail.com>
2024-02-02 13:59:06 +0100

    Fix migration and schema validation order issue
    Before this fix, Spring boot runs schema validation before Flyway migration

---
+++ src/main/java/ua/mibal/booking/config/db/FlywayBeanPostProcessor.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024. Mykhailo Balakhon mailto:9mohapx9@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ua.mibal.booking.config.db;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+// TODO
+/**
+ * Custom {@link FlywayBeanPostProcessor} used to start Flyway migration
+ * just after {@link Flyway} bean initialization
+ *
+ * {@link FlywayBeanPostProcessor} used only in {@code "production"} Spring profile
+ * because of different schema validation and db migration order
+ * at Spring boot app startup
+ *
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
+ */
+@Profile("heroku")
+@Component
+public class FlywayBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean,
+                                                 String beanName) throws BeansException {
+        if (bean instanceof Flyway flyway) {
+            flyway.migrate();
+        }
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+}
