cfc995d8c32e1a02704dc36d486e508a3f331056
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-02-20 11:35:06 +0100

    Move `DateRangeValidator_UnitTest` to `application.validation`

--- src/test/java/ua/mibal/booking/application/validation/DateRangeValidator_UnitTest.java
+++ src/test/java/ua/mibal/booking/application/validation/DateRangeValidator_UnitTest.java
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 
-package ua.mibal.booking.model.validation;
+package ua.mibal.booking.application.validation;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import ua.mibal.booking.application.validation.DateRangeValidator;
 import ua.mibal.booking.domain.DateRangeValidRequest;
 
 import java.time.LocalDate;
