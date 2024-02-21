705ab63acc9a076cc7a7175975a85483278eb37f
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-02-21 11:50:44 +0100

    Fix `ApplicationProps.locale` validation

--- src/main/java/ua/mibal/booking/config/properties/ApplicationProps.java
+++ src/main/java/ua/mibal/booking/config/properties/ApplicationProps.java
@@ -17,6 +17,7 @@
 package ua.mibal.booking.config.properties;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -35,7 +36,7 @@ public record ApplicationProps(
         @NotBlank
         String email,
 
-        @NotBlank
+        @NotNull
         Locale locale
 ) {
 }
