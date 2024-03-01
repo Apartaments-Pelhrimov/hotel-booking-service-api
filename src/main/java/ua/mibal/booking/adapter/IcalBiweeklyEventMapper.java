094aded086f617ba38f950087ef0c3a29a5e87f2
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-01 15:39:29 +0100

    Implement `IcalBiweeklyEventMapper` with tests
    `IcalBiweeklyEventMapper` should map dates from UTC to local Zone id (settings at `application.yaml`)

--- src/main/java/ua/mibal/booking/adapter/IcalBiweeklyEventMapper.java
+++ src/main/java/ua/mibal/booking/adapter/IcalBiweeklyEventMapper.java
@@ -17,25 +17,59 @@
 package ua.mibal.booking.adapter;
 
 import biweekly.component.VEvent;
+import biweekly.property.DateOrDateTimeProperty;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import ua.mibal.booking.config.properties.CalendarProps;
+import ua.mibal.booking.domain.DefaultEvent;
 import ua.mibal.booking.domain.Event;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
+@RequiredArgsConstructor
 @Component
 public class IcalBiweeklyEventMapper {
+    private final CalendarProps calendarProps;
 
-    // TODO
     public List<VEvent> toIcalEvents(List<Event> events) {
-        return null;
+        return events.stream()
+                .map(this::toIcalEvent)
+                .toList();
     }
 
-    // TODO
     public List<Event> toEvents(List<VEvent> vevents) {
-        return null;
+        return vevents.stream()
+                .map(this::toEvent)
+                .toList();
+    }
+
+    private VEvent toIcalEvent(Event event) {
+        VEvent vevent = new VEvent();
+        vevent.setDateStart(toUtcDate(event.getStart()));
+        vevent.setDateEnd(toUtcDate(event.getEnd()));
+        vevent.setSummary(event.getEventName());
+        return vevent;
+    }
+
+    private Event toEvent(VEvent vevent) {
+        DefaultEvent event = new DefaultEvent();
+        event.setEventName(vevent.getSummary().getValue());
+        event.setStart(toLocalDate(vevent.getDateStart()));
+        event.setEnd(toLocalDate(vevent.getDateEnd()));
+        return event;
+    }
+
+    private Date toUtcDate(LocalDateTime local) {
+        return Date.from(local.atZone(calendarProps.zoneId()).toInstant());
+    }
+
+    private LocalDateTime toLocalDate(DateOrDateTimeProperty utcDate) {
+        return LocalDateTime.ofInstant(utcDate.getValue().toInstant(), calendarProps.zoneId());
     }
 }
