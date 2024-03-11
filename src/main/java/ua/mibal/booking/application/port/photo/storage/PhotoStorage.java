9c9b8f165ece0ae3c604ed7a53d0354f55aa387a
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-11 15:15:23 +0100

    Add javadocs for `PhotoStorage`

--- src/main/java/ua/mibal/booking/application/port/photo/storage/PhotoStorage.java
+++ src/main/java/ua/mibal/booking/application/port/photo/storage/PhotoStorage.java
@@ -20,15 +20,36 @@ import org.springframework.web.multipart.MultipartFile;
 import ua.mibal.booking.application.port.photo.storage.model.PhotoResource;
 
 /**
+ * The {@code PhotoStorage} interface defines the contract for a service responsible for
+ * photo storage.
+ * Implementations of this interface are expected to provide functionality for store
+ * photos by unique keys.
+ *
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
-// TODO add javadoc
 public interface PhotoStorage {
 
+    /**
+     * Retrieves a {@code PhotoResource} based on the specified key.
+     *
+     * @param key The unique key associated with the desired photo.
+     * @return A {@code PhotoResource} representing the requested photo.
+     */
     PhotoResource getPhotoBy(String key);
 
+    /**
+     * Uploads a photo provided as a {@code MultipartFile} to the photo storage.
+     *
+     * @param photo The {@code MultipartFile} representing the photo to be uploaded.
+     * @return A {@code String} representing the unique key assigned to the uploaded photo.
+     */
     String uploadPhoto(MultipartFile photo);
 
+    /**
+     * Deletes a photo from storage based on the specified key.
+     *
+     * @param key The unique key associated with the photo to be deleted.
+     */
     void deletePhotoBy(String key);
 }
