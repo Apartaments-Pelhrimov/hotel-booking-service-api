8599e09c2d5d5f55d71d5bc34e226678eef788bf
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-03-11 15:24:35 +0100

    Add javadocs for `ReservationSystem`

--- src/main/java/ua/mibal/booking/application/port/reservation/system/ReservationSystem.java
+++ src/main/java/ua/mibal/booking/application/port/reservation/system/ReservationSystem.java
@@ -23,13 +23,35 @@ import ua.mibal.booking.domain.Event;
 import java.util.List;
 
 /**
+ * The {@code ReservationSystem} interface defines the contract for an integration with the
+ * appropriate reservation system.
+ * Responsible for retrieving events from another system and checking whether the instance is
+ * available for a given reservation.
+ *
  * @author Mykhailo Balakhon
  * @link <a href="mailto:9mohapx9@gmail.com">9mohapx9@gmail.com</a>
  */
 public interface ReservationSystem {
 
+    /**
+     * Retrieves a list of {@code Event} objects associated with the specified
+     * {@code ApartmentInstance}.
+     *
+     * @param apartmentInstance The {@code ApartmentInstance} for which events are to be retrieved.
+     * @return A {@code List} of {@code Event} objects representing events associated with the
+     * apartment instance.
+     */
     List<Event> getEventsFor(ApartmentInstance apartmentInstance);
 
+    /**
+     * Checks whether the specified {@code ApartmentInstance} is available for reservation based on
+     * the provided {@code ReservationForm} with reservation range.
+     *
+     * @param apartmentInstance The {@code ApartmentInstance} to be checked for availability.
+     * @param reservationForm   The {@code ReservationForm} containing details for the reservation.
+     * @return {@code true} if the apartment instance is available for reservation;
+     * {@code false} otherwise.
+     */
     boolean isFreeForReservation(ApartmentInstance apartmentInstance,
                                  ReservationForm reservationForm);
 }
